c211a7c7c0aaf55090747d10fda12235eac54853
Pauline <pauline.lehoux@etu.unilim.fr>
2017-11-25 23:21:09 +0800

    Pages poster et visualiser les annonces
    Hello,
    Comme discuté j’ai créé 2 pages permettant de déposer une annonce et de
    visualiser les annonces.
    C’est pas le top de l’ergonomie, je ne suis pas parvenue à faire
    exactement ce que je voulais. Mais le plus gros est là ^^
    
    Je me demande notamment comment ajouter un champ”photo” dans le
    formulaire pour que l’utilisateur puisse uploader des photos de son
    bien. Est-ce possible ?
    
    Pour la page de visualisation, j’avais pensé proposer une carousel en
    top de page puis un format de “cartes” pour présenter les différentes
    annonces. Qu’en pensez-vous ?
    
    Je peux retravailler dessus demain pour améliorer selon vos conseils.
    Merci par avance !

---
+++ annonces-post.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/custom.css">
+  </head>
+  <body>
+  <div class="container">
+    <div class="text-center">
+      <h1>Uchi</h1>
+      <small class="text-muted">Agence Immobilière</small>
+    </div>
+      </div>
+      
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Publiez votre annonce</h2>
+        <div class="col-auto">
+      <label class="form-signin" for="type">Type de bien</label>
+    <select class="custom-select">
+  <option selected>Sélectionnez le type de bien</option>
+        <option value="1">Appartement</option>
+        <option value="2">Maison</option>
+        <option value="4">Château</option>
+        <option value="5">Local commercial</option>
+        <option value="6">Parking/box</option>
+    </select>  
+    </div>
+          <br>
+ <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputSurface">Surface</label>
+      <input type="text" class="form-control" id="inputSurface" placeholder="Surface">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPrice">Prix</label>
+      <input type="text" class="form-control" id="inputPrice" placeholder="Prix">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Adresse</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Adresse postale">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">Ville</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Code postal</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
+  <div class="form-group">
+  </div>
+<button type="submit" class="btn btn-lg btn-primary btn-block">Valider et envoyer l'annonce</button>
+</form>
+      
+      
+      
+      
+       <script src="/js/jquery-3.2.1.min.js"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
---
+++ annonces_view.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/custom.css">
+  </head>
+  <body>
+  <div class="container">
+    <div class="text-center">
+      <h1>Uchi</h1>
+      <small class="text-muted">Agence Immobilière</small>
+    </div>
+      </div>
+      
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="">
+    </div>
+  </div>
+          <div class="carousel-item">
+  <img src="..." alt="...">
+  <div class="carousel-caption d-none d-md-block">
+    <h3>Découvrez tous nos biens</h3>
+    <p>En savoir plus en un clic !</p>
+  </div>
+</div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+      
+      
+        <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Nom du bien</h4>
+        <p class="card-text">Description du bien.</p>
+        <a href="#" class="btn btn-primary">Voir le détail du bien</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Nom du bien</h4>
+        <p class="card-text">Description du bien.</p>
+        <a href="#" class="btn btn-primary">Voir le détail du bien</a>
+      </div>
+    </div>
+  </div>
+</div>
+       <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Nom du bien</h4>
+        <p class="card-text">Description du bien.</p>
+        <a href="#" class="btn btn-primary">Voir le détail du bien</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Nom du bien</h4>
+        <p class="card-text">Description du bien.</p>
+        <a href="#" class="btn btn-primary">Voir le détail du bien</a>
+      </div>
+    </div>
+  </div>
+</div>
+      
+      
+       <script src="/js/jquery-3.2.1.min.js"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
