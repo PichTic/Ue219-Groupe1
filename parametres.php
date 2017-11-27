d0535dc3352d2407df4d82be84e3314127fc20ec
MAXSDEUST <33908606+MAXSDEUST@users.noreply.github.com>
2017-11-27 10:41:28 +0100

    Add files via upload
    J'ai crée la page paramètres maintenant je vais me pencher sur le php de la page pour modifier les infos et tout ça.

---
+++ parametres.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Paramètres du compte</title>
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/custom.css">
+    </head>
+    <body>
+        <div class="right">
+            <div id="recap">
+                <p>Votre Nom est :</p>
+                <p>Votre Prénom est :</p>
+                <p>Vous avez : <span>biens en vente</span></p>
+                <a>Voir vos biens</a>
+            </div>
+            <form action="" method="post">
+                <fieldset>
+                    <legend>Modifier vos informations</legend>*
+                    <label>Nom :</label>
+                    <input type="text"><br>
+                    <label>Prenom :</label>
+                    <input type="text"><br>
+                    <label>Téléphone</label>
+                    <input type="text"><br>
+                    <label>E-mail</label>
+                    <input type="text"><br>
+                    <label>Mot de passe</label>
+                    <input type="text"><br>
+                    <input type="button" value="Valider">
+                </fieldset>
+            </form>
+        </div>
+    </body>
+</html>
